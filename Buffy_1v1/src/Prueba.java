315d52ce840d9d1a8ec5ec009cffae897ef15519
Rudaco2001 <73298874+Rudaco2001@users.noreply.github.com>
2020-10-26 21:04:47 +0100

    Update Prueba.java

--- Buffy_1v1/src/Prueba.java
+++ Buffy_1v1/src/Prueba.java
@@ -1,9 +1,25 @@
 
-public class Prueba {
+public class Vampire {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private int damage = 1; 
+	private int health = 5;
+	private int pos_X;
+	private int pos_Y;
+	public void spawn (int Dimension_X, int Dimension_Y) {
+		pos_X = Dimension_X;
+		
+	}
+	public void takeDamage (int damage) {
+		health -= damage;
+	}
+	
+	public int Damage() {
+		return damage;
+	}
+	
+	public void Advance(int ciclo) {
+		if (ciclo % 2 == 0)
+		pos_X--;
 	}
 
 }
