a83ac70da900ac881cdc34a0255f154498d93921
Rudaco2001 <73298874+Rudaco2001@users.noreply.github.com>
2020-10-26 21:26:39 +0100

    nani

--- Buffy_1v1/src/Vampire.java
+++ Buffy_1v1/src/Vampire.java
@@ -2,7 +2,7 @@
 public class Vampire {
 
 	private int damage = 1; 
-	private int health = 6;
+	private int health = 5;
 	private int pos_X;
 	private int pos_Y;
 	public void spawn (int Dimension_X, int Dimension_Y) {
