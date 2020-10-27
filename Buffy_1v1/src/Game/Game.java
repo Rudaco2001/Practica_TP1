e4495a2a34a29e4cfaf3f80a0bb4e5b0e323f96c
Rudaco2001 <73298874+Rudaco2001@users.noreply.github.com>
2020-10-27 18:38:07 +0100

    Create Game.java

---
+++ Buffy_1v1/src/Game/Game.java
@@ -0,0 +1,18 @@
+package Game;
+
+import java.util.Random;
+
+public class Game {
+
+	public final static int BOARD_LENGTH = 10;
+	public final static int BOARD_WIDTH = 4;
+	private Random rand;
+	private characters.Vampire Vampire;
+	private characters.Player player;
+	private characters.Slayer slayer;
+	
+		
+
+	
+
+}
