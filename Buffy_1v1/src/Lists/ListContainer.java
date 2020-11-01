ebfa69d091ee97eab14509da01a683881039ea98
Rudaco2001 <73298874+Rudaco2001@users.noreply.github.com>
2020-11-01 19:14:05 +0100

    Update ListContainer.java

--- Buffy_1v1/src/Lists/ListContainer.java
+++ Buffy_1v1/src/Lists/ListContainer.java
@@ -5,10 +5,10 @@ import Lists.SlayerList;
 public class ListContainer {
 	VampireList vampirelist;
 	SlayerList slayerlist;
-	public ListContainer(int maxVampires, int maxSlayers)
+	public ListContainer(int maxVampires, int maxSlayers, Game game)
 	{
-		vampirelist = new VampireList(maxVampires);
-		slayerlist = new SlayerList(maxSlayers);
+		vampirelist = new VampireList(maxVampires, game);
+		slayerlist = new SlayerList(maxSlayers, game);
 	}
 	public boolean isSlayerList(int i,int j) {
 		return slayerlist.isSlayerList(i, j);
@@ -16,10 +16,28 @@ public class ListContainer {
 	public boolean isVampireList(int i,int j) {
 		return vampirelist.isVampireList(i, j);
 	}
-	public void addVampire(int v_i, int v_j) {
-		vampirelist.addVampire(v_i, v_j);
+	public void addVampire() {
+		vampirelist.addVampire();
 	}
 	public void addSlayer(int s_i,int s_j) {
 		slayerlist.addSlayer(s_i, s_j);
+	}
+	public void moveVampire() {
+		vampirelist.moveVampires();
+	}
+	public String VampireString(int i, int j) {
+		return vampirelist.VampireString(i, j);
+	}
+	public String SlayerString(int i, int j) {
+		return slayerlist.SlayerString(i, j);
+	}
+	public void SlayerAttack() {
+		
+		slayerlist.SlayerAttack();
+		
+	}
+	public void VampireTakeDamage(int i, int j, int damage) {
+		vampirelist.VampireTakeDamage(i, j, damage);
+		
 	}
 }
