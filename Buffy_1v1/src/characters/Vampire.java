2fffd39c622ebe8a2503099b95c96676b41caa5b
Rudaco2001 <73298874+Rudaco2001@users.noreply.github.com>
2020-11-01 19:14:00 +0100

    Update Vampire.java

--- Buffy_1v1/src/characters/Vampire.java
+++ Buffy_1v1/src/characters/Vampire.java
@@ -1,19 +1,22 @@
 package characters;
 
+import logic.Game;
+
 public class Vampire {
 	private int damage = 1; 
 	private int health = 5;
 	private int pos_X;
 	private int pos_Y;
-	public Vampire(int i, int j) {
+	private int cicloI;
+	private Game game;
+	public Vampire(int i, int j, Game game) {
 		// TODO Auto-generated constructor stub
+		this.game = game;
 		pos_X = i;
 		pos_Y = j;
+		cicloI = this.game.GetCicle() % 2;
 	}
-	public void spawn (int Dimension_X, int Dimension_Y) {
-		pos_X = Dimension_X;
-		
-	}
+	
 	public void takeDamage (int damage) {
 		health -= damage;
 	}
@@ -22,18 +25,20 @@ public class Vampire {
 		return damage;
 	}
 	
-	public void Advance(int ciclo) {
-		if (ciclo % 2 == 0)
-		pos_X--;
+	public void moveVampire() {
+		
+		if (game.GetCicle() % 2 == cicloI) {
+			if (!game.isVampire(pos_X, pos_Y - 1)&& !game.isSlayer(pos_X, pos_Y -1))
+			pos_Y--;
+		}
 	}
 	public String toString() {
-		return "V";
+		return ("V[" + health + "]") ;
 	}
 	public boolean isVampire(int i, int j) {
-		if((i == pos_X)&&(j == pos_Y))
-			return true;
-		else
-			return false;			
+		
+			return i == pos_X && j == pos_Y;
+				
 	}
 }
 
