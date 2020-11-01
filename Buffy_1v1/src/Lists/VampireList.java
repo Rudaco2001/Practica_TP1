f0d265c293a0d2784ec2590451b20dff9eac050b
Rudaco2001 <73298874+Rudaco2001@users.noreply.github.com>
2020-11-01 19:14:09 +0100

    Update VampireList.java

--- Buffy_1v1/src/Lists/VampireList.java
+++ Buffy_1v1/src/Lists/VampireList.java
@@ -1,27 +1,56 @@
 package Lists;
 import characters.Vampire;
+import logic.Game;
 public class VampireList {
 	private Vampire[] vampirelist;
 	private int nVampire = 0;
-	public VampireList(int maxVampire) {
+	Game game;
+	public VampireList(int maxVampire, Game game) {
 		vampirelist = new Vampire[maxVampire];
-		
+		this.game = game;
 	}
-	public void addVampire(int i, int j) {
+	public void addVampire() {
 		if(nVampire < vampirelist.length)
 		{
-			vampirelist[nVampire] = new Vampire(j, i);
-			nVampire++;
+			int spawn_i = game.randReadint(game.BOARD_WIDTH);
+			System.out.println("Intentando vampiro en " + spawn_i + " " + game.BOARD_LENGTH);
+			if (!game.isVampire(spawn_i, game.BOARD_LENGTH - 1)) { 
+				vampirelist[nVampire] = new Vampire(spawn_i, game.BOARD_LENGTH - 1, game);
+				nVampire++;
+				System.out.println("Creando vampiro en " + spawn_i + " " + game.BOARD_LENGTH);
+			}
 			
 		}
 	}
 	public boolean isVampireList(int i, int j) {
 		boolean encontrado = false;
 		for	(int i1 = 0; i1 < nVampire; i1++ ) {
-			if (vampirelist[i1].isVampire(j, i))
+			if (vampirelist[i1].isVampire(i, j))
 				encontrado = true;
 		}
 		return encontrado;
 		
+	}
+	public void moveVampires() {
+		for	(int i2 = 0; i2 < nVampire; i2++ ) {
+			vampirelist[i2].moveVampire();
+		}
+	}
+	public String VampireString(int i, int j) {
+		for	(int i1 = 0; i1 < nVampire; i1++ ) {
+			if (vampirelist[i1].isVampire(i, j))
+				return vampirelist[i1].toString();
+		}
+		return null;
+	}
+	public void VampireTakeDamage(int i, int j, int damage) {
+		for	(int i1 = 0; i1 < nVampire; i1++ ) {
+			if (vampirelist[i1].isVampire(i, j))
+			{
+				vampirelist[i1].takeDamage(damage);
+				break;
+			}
+		
+		}
 	}
 }
