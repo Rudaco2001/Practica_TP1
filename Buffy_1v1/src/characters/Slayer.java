a39d2b2f429ec70419965e335ee3ee91c76b00f6
Rudaco2001 <73298874+Rudaco2001@users.noreply.github.com>
2020-11-01 19:13:58 +0100

    Update Slayer.java

--- Buffy_1v1/src/characters/Slayer.java
+++ Buffy_1v1/src/characters/Slayer.java
@@ -1,14 +1,18 @@
 package characters;
 
+import logic.Game;
+
 public class Slayer {
 	private int damage = 1; 
 	private int health = 5;
 	private int pos_X;
 	private int pos_Y;
+	private Game game;
 	
-	public Slayer(int i,int j) {
+	public Slayer(int i,int j,Game game) {
 		pos_X = i;
 		pos_Y = j;
+		this.game = game;
 	}
 	public void takeDamage (int damage) {
 		health -= damage;
@@ -22,7 +26,16 @@ public class Slayer {
 		return i == pos_X && j == pos_Y;
 	}
 	public String toString() {
-		return "S";
+		return "S[" + health + "]";
+	}
+	public void Attack() {
+		for (int j = pos_Y + 1; j < game.BOARD_LENGTH; j++) {
+			if(game.isVampire(pos_X, j)) {
+				game.VampireTakeDamage(pos_X, j, damage);
+				break;
+			}
+		}
+		
 	}
 	
 
