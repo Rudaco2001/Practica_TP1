56ebef36831f287d2a5406f472747455e2f4a10b
Rudaco2001 <73298874+Rudaco2001@users.noreply.github.com>
2020-11-06 20:22:47 +0100

    Update Vampire.java

--- Buffy_1v1/src/characters/Vampire.java
+++ Buffy_1v1/src/characters/Vampire.java
@@ -7,14 +7,13 @@ public class Vampire {
 	private int health = 5;
 	private int pos_X;
 	private int pos_Y;
-	private int cicloI;
+	private int cicloI = 1;
 	private Game game;
 	public Vampire(int i, int j, Game game) {
 		// TODO Auto-generated constructor stub
 		this.game = game;
 		pos_X = i;
-		pos_Y = j;
-		cicloI = this.game.GetCicle() % 2;
+		pos_Y = j;		
 	}
 	
 	public void takeDamage (int damage) {
@@ -27,10 +26,15 @@ public class Vampire {
 	
 	public void moveVampire() {
 		
-		if (game.GetCicle() % 2 == cicloI) {
-			if (!game.isVampire(pos_X, pos_Y - 1)&& !game.isSlayer(pos_X, pos_Y -1))
+		if (cicloI % 2 == 0) {
+			if (!game.isVampire(pos_X, pos_Y - 1)&& !game.isSlayer(pos_X, pos_Y -1)) {
 			pos_Y--;
+			cicloI++;
+			}
 		}
+		else 
+			cicloI++;
+		
 	}
 	public String toString() {
 		return ("V[" + health + "]") ;
@@ -43,6 +47,11 @@ public class Vampire {
 
 	public boolean isDead() {		
 		return health <= 0;
+	}
+
+	public void Attack() {
+		if (game.isSlayer(pos_X, pos_Y - 1))
+			game.SlayerTakeDamage(damage, pos_X, pos_Y - 1);		
 	}
 }
 
